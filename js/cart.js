11a015484ef11a092a9bfc773fb6b687263e6828
VuongNgocMinh <vuongminh17112003@gmail.com>
2023-07-21 15:49:44 +0700

    suadoilan4

---
+++ js/cart.js
@@ -0,0 +1,104 @@
+export const cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+export const cartQuantity = document.querySelector(".cart-logo .cart-ammount");
+export const cartPrice = document.querySelector(".cart .cart-price");
+export const woocommerPrice = document.querySelector(
+  ".total-products .row .total-value"
+);
+const infoProducts = document.querySelector(
+  ".information-products .infomation-content"
+);
+// =========================================================
+export function render(cartData) {
+  if (infoProducts) {
+    const content = cartData
+      .map((item, index) => {
+        let itemSum = convertNumber(item.price) * item.amount;
+        let itemPrice = convertNumber(item.price);
+        return `
+        <tr class="item">
+          <td class="product-name">
+          <img  src="${item.image}" alt="" class="product-img">
+            <p>${item.name}</p>
+          </td>
+          <td class="product-price">
+            <p>${itemPrice.toLocaleString()}</p>
+          </td>
+          <td class="product-quantity">
+            <input
+              type="number" name="quantity"  min="1" max="100" value="${
+                item.amount
+              }"
+            />
+          </td>
+          <td class="product-sum">
+            <p>${itemSum.toLocaleString()}</p>
+          </td>
+          <td >
+            <button id=${index} class="product-delete">Hủy</button>
+          </td>
+        </tr> `;
+      })
+      .join("");
+    infoProducts.innerHTML = content;
+    cartQuantity.textContent = cartData.length;
+  }
+}
+export function saveData(cartData) {
+  localStorage.setItem("cartData", JSON.stringify(cartData));
+}
+function deleteProduct(cartData) {
+  if (infoProducts) {
+    infoProducts.addEventListener("click", (e) => {
+      const deleteBtn = e.target.closest(".product-delete");
+      const btnId = parseInt(e.target.id);
+      if (deleteBtn) {
+        cartData.splice(btnId, 1);
+        render(cartData);
+        updatePrice(cartData);
+        saveData(cartData);
+      }
+    });
+  }
+}
+function updatePrice(cartData) {
+  let sumProductList = 0;
+  cartData.forEach((item, index) => {
+    sumProductList += convertNumber(item.price);
+  });
+  if (woocommerPrice) {
+    woocommerPrice.textContent = sumProductList.toLocaleString();
+  }
+  if (cartPrice) {
+    cartPrice.textContent = sumProductList.toLocaleString();
+  }
+}
+function updateBtn(cartData) {
+  const updateBtn = document.querySelector(".updateProduct");
+  if (updateBtn) {
+    updateBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const quantityInput = document.querySelectorAll(
+        ".product-quantity input"
+      );
+      let sumProductList = 0;
+      cartData.forEach((item, index) => {
+        let newAmount = parseInt(quantityInput[index].value);
+        quantityInput[index].value = newAmount;
+        item.amount = newAmount;
+        sumProductList += convertNumber(item.price) * item.amount;
+        woocommerPrice.textContent = sumProductList.toLocaleString();
+        cartPrice.textContent = sumProductList.toLocaleString();
+      });
+      render(cartData);
+      saveData(cartData);
+    });
+  }
+}
+function convertNumber(number) {
+  return (number = Number(number.replaceAll(",", "")));
+}
+render(cartData);
+deleteProduct(cartData);
+updatePrice(cartData);
+updateBtn(cartData);
+cartQuantity.textContent = cartData.length;
--- js/products.js
+++ js/products.js
@@ -1,17 +1,9 @@
 import { productData } from "./data.js";
+import { cartData, cartQuantity } from "./cart.js";
+import { saveData, render } from "./cart.js";
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category"); // nếu url có ?category=man category-man
-
 const productList = document.querySelector(".product-list");
-const cartData = JSON.parse(localStorage.getItem("cartData")) || [];
-const infoProducts = document.querySelector(
-  ".information-products .infomation-content"
-);
-const cartQuantity = document.querySelector(".cart-logo .cart-ammount");
-const cartPrice = document.querySelector(".cart .cart-price");
-const woocommerPrice = document.querySelector(
-  ".total-products .row .total-value"
-);
 productData.forEach((product) => {
   if (product.category === category) {
     const productItem = document.createElement("section");
@@ -43,13 +35,18 @@ productData.forEach((product) => {
     addToCartButton.textContent = "THÊM VÀO GIỎ";
     addToCartButton.classList.add("item-add");
 
+    const showCart = document.createElement("a");
+    showCart.href = "./product_payment.html";
+    showCart.textContent = "XEM GIỎ HÀNG";
+    showCart.classList.add("show-add");
+    showCart.style.display = "none";
     //name
     productNameLink.appendChild(productName);
     // content
     itemContent.appendChild(productNameLink);
     itemContent.appendChild(productPrice);
     itemContent.appendChild(addToCartButton);
-
+    itemContent.appendChild(showCart);
     // a link
     productLink.appendChild(productImage);
     productLink.appendChild(itemContent);
@@ -63,90 +60,28 @@ productData.forEach((product) => {
       if (existingItem) {
         existingItem.amount += 1;
         product.amount = existingItem.amount;
+        addToCartButton.classList.toggle("hidden");
+        showCart.classList.toggle("show");
+
         render(cartData);
       } else {
-        isEmpty(cartData, product);
+        if (isExist(cartData, product)) {
+          cartData.push(product);
+          addToCartButton.classList.toggle("hidden");
+          showCart.classList.toggle("show");
+        }
         cartQuantity.textContent = cartData.length;
       }
       saveData(cartData);
     });
+    const existItem = cartData.find((item) => item.id === product.id);
+    if (existItem) {
+      addToCartButton.classList.toggle("hidden");
+      showCart.classList.toggle("show");
+    }
   }
 });
-render(cartData);
-deleteProduct(cartData);
-updatePrice(cartData);
-cartQuantity.textContent = cartData.length;
-
-const updateBtn = document.querySelector(".updateProduct");
-if (updateBtn) {
-  updateBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const quantityInput = document.querySelectorAll(".product-quantity input");
-    let sumProductList = 0;
-    cartData.forEach((item, index) => {
-      let newAmount = parseInt(quantityInput[index].value);
-      quantityInput[index].value = newAmount;
-      item.amount = newAmount;
-      sumProductList += convertNumber(item.price) * item.amount;
-      woocommerPrice.textContent = sumProductList;
-      cartPrice.textContent = sumProductList;
-    });
-
-    render(cartData);
-    saveData(cartData);
-  });
-}
-function render(cartData) {
-  if (infoProducts) {
-    const content = cartData
-      .map((item, index) => {
-        return `
-      <tr class="item">
-        <td class="product-name">
-        <img  src="${item.image}" alt="" class="product-img">
-          <p>${item.name}</p>
-        </td>
-        <td class="product-price">
-          <p>${convertNumber(item.price)}</p>
-        </td>
-        <td class="product-quantity">
-          <input
-            type="number" name="quantity"  min="1" max="100" value="${
-              item.amount
-            }"
-          />
-        </td>
-        <td class="product-sum">
-          <p>${convertNumber(item.price) * item.amount}</p>
-        </td>
-        <td >
-          <button id=${index} class="product-delete">Hủy</button>
-        </td>
-      </tr> `;
-      })
-      .join("");
-    infoProducts.innerHTML = content;
-    cartQuantity.textContent = cartData.length;
-  }
-}
-function deleteProduct(cartData) {
-  if (infoProducts) {
-    infoProducts.addEventListener("click", (e) => {
-      const deleteBtn = e.target.closest(".product-delete");
-      const btnId = parseInt(e.target.id);
-      if (deleteBtn) {
-        cartData.splice(btnId, 1);
-        render(cartData);
-        updatePrice(cartData);
-        saveData(cartData);
-      }
-    });
-  }
-}
-function saveData(cartData) {
-  localStorage.setItem("cartData", JSON.stringify(cartData));
-}
-function isEmpty(cartData, item) {
+function isExist(cartData, item) {
   let empty = true;
   for (const i of cartData) {
     if (i.id === item.id) {
@@ -156,21 +91,6 @@ function isEmpty(cartData, item) {
   }
   if (empty === true) {
     alert("Thêm sản phẩm thành công");
-    cartData.push(item);
-  }
-}
-function convertNumber(number) {
-  return (number = Number(number.replaceAll(",", "")));
-}
-function updatePrice(cartData) {
-  let sumProductList = 0;
-  cartData.forEach((item, index) => {
-    sumProductList += convertNumber(item.price);
-  });
-  if (woocommerPrice) {
-    woocommerPrice.textContent = sumProductList;
-  }
-  if (cartPrice) {
-    cartPrice.textContent = sumProductList;
+    return true;
   }
 }
